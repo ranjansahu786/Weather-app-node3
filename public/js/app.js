2ff9d9c99325ce8917254a0eba157fc1dbaf833a
Sonu Nayak <sonunayak@Tejendra-sahu-macbook.local>
2020-01-14 14:49:08 +0530

    removed console.log from app.js and changed footer statement

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('client side java script')
-
 const weartherform = document.querySelector('form')
 const address = document.querySelector('input')
 const messageone=document.querySelector('#message-1')
